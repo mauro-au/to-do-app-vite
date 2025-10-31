9a3376c0cddf71f9c52f8299229006ea72827b4c
mauro-au <mauricio.a.developer@gmail.com>
2025-10-30 23:53:57 -0300

    Se agregan los métodos necesarios para usar en la aplicación

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -16,5 +16,72 @@ const state = {
 }
 
 const initStore = () => {
-  console.log('InitStore ')
+  console.log(state);
+  console.log('InitStore ');
+}
+
+const loadStore = () => {
+  throw new Error('Not Implemented');
+}
+
+const getTodos = ( filter = Filter.All ) => {
+  switch (filter) {
+    case Filter.All:
+      return [...state.todos];
+
+    case Filter.Completed:
+      return state.todos.filter(todo => todo.done);
+
+    case Filter.Pending:
+      return state.todos.filter(todo => !todo.done);
+
+    default:
+      throw new Error(`Option ${ filter } is not valid`);
+  }
+}
+
+/**
+ *
+ * @param {String} description
+ */
+const addTodo = (description) => {
+  if (!description) throw new Error('Description is required');
+  state.todos.push(new Todo(description));
+}
+
+const toggleTodo = (todoId) => {
+  state.todos = state.todos.map(todo => {
+    if (todo.id === todoId) {
+      todo.done = !todo.done;
+    }
+    return todo;
+  })
+}
+
+const deleteTodo = (todoId) => {
+  state.todos = state.todos.filter(todo => todo.id !== todoId);
+}
+
+const deleteCompleted = () => {
+  state.todos = state.todos.filter(todo => todo.done);
+}
+
+const setFilter = (newFilter = Filter.All) => {
+  state.filter = newFilter
+}
+
+const getCurrentFilter = () => {
+  return state.filter;
+}
+
+export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  getTodos,
+  initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 }
--- src/todos/models/todo.models.js
+++ src/todos/models/todo.models.js
@@ -1,10 +1,10 @@
-
+import { v4 as uuid } from 'uuid';
 export class Todo {
   /**
    * @param {String} description
    */
   constructor(description) {
-    this.id = 1;
+    this.id = uuid();
     this.description = description;
     this.done = false;
     this.createdAt = new Date();
